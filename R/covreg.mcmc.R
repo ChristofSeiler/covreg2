edfe237cd175bc9b4d56a431077d0f48b839d016
ChristofSeiler <christof.seiler@gmail.com>
2016-10-08 14:56:19 -0700

    added possibility to initialize B1, B2, and A

--- R/covreg.mcmc.R
+++ R/covreg.mcmc.R
@@ -1,5 +1,5 @@
 covreg.mcmc <-
-function(fmean,fcov,data=NULL,R=1,niter=10000,nthin=10,nsave=niter/nthin,verb=T){
+function(fmean,fcov,data=NULL,R=1,niter=10000,nthin=10,nsave=niter/nthin,verb=T,B1=NULL,B2=NULL,A=NULL){
 
 ## X1 is the covariate for the mean model ##
 ## X2 is the covariate for the cov model ##
@@ -17,11 +17,17 @@ q2=dim(X2)[2]
 
 #Y=scale(Y,scale=F)
 ## starting values ##
-B2=array(0,dim=c(p,q2,R))
+if(is.null(B2)) {
+  B2=array(0,dim=c(p,q2,R))
+}
 f0=lm(fmean,data)
-B1=t(f0$coef)
+if(is.null(B1)) {
+  B1=t(f0$coef)
+}
 G=matrix(rnorm(n*R),ncol=R)
-A=cov(f0$resid)
+if(is.null(A)) {
+  A=cov(f0$resid)
+}
 iA=solve(A)
 
 
@@ -52,33 +58,33 @@ for (ns in 1:niter){
 
 ## full conditionals ##
 
-## update B2 and G 
+## update B2 and G
 for(k in sample(1:R)){
-	E<-Y-X1%*%t(B1) 
+	E<-Y-X1%*%t(B1)
 	#E=Y
-    	for(l in (1:R)[-k]) { E<-E-(X2%*% t(B2[,,l]))*G[,l]  } 
+    	for(l in (1:R)[-k]) { E<-E-(X2%*% t(B2[,,l]))*G[,l]  }
     	s2g<-1/( 1+  diag( X2%*%t(B2[,,k])%*%iA%*%B2[,,k]%*%t(X2)) )
-    	eg<-s2g*diag(  X2%*%t(B2[,,k])%*%iA%*%t(E) )  
+    	eg<-s2g*diag(  X2%*%t(B2[,,k])%*%iA%*%t(E) )
     	G[,k]<-rnorm(n,eg,sqrt(s2g))
-     
+
     	Xg<-diag(G[,k])%*%X2
     	Bn<- (t(E)%*%Xg)%*%solve(( t(Xg)%*%Xg + iV02 ) )
-    	B2[,,k]<-rmn(Bn, A , solve( t(Xg)%*%Xg + iV02 )) 
+    	B2[,,k]<-rmn(Bn, A , solve( t(Xg)%*%Xg + iV02 ))
 }
-  ## 
+  ##
 
 ## update A and B1 conditional on B2 and G
 E<-Y
-SSB<-matrix(0,p,p) 
-for(k in 1:R){ 
-	E<- E - (X2%*% t(B2[,,k]))*G[,k] 
+SSB<-matrix(0,p,p)
+for(k in 1:R){
+	E<- E - (X2%*% t(B2[,,k]))*G[,k]
     	SSB<-SSB+ B2[,,k]%*%iV02%*%t(B2[,,k])
 }
-Bn=(t(E)%*%X1+B01%*%iV01)%*%solve(t(X1)%*%X1 + iV01 ) 
+Bn=(t(E)%*%X1+B01%*%iV01)%*%solve(t(X1)%*%X1 + iV01 )
 B1=rmn(Bn,A,solve(t(X1)%*%X1+iV01))
 E=E-X1%*%t(B1)
 SSB=SSB+(B1-B01)%*%iV01%*%t(B1-B01)
-An<-A0 + t(E)%*%E + SSB 
+An<-A0 + t(E)%*%E + SSB
 iA<-rwish( solve(An), nu0 + n + q1+q2*R  ) ; A<-solve(iA)
 
 
@@ -89,7 +95,7 @@ b1.save[,,ns/nthin]=B1
 b2.save[,,,ns/nthin]=B2
 a.save[,,ns/nthin]=A
 }
-if (verb==T & ns%%(niter/100)==0)cat(round(ns/niter*100),"% done",date(), "\n") 
+if (verb==T & ns%%(niter/100)==0)cat(round(ns/niter*100),"% done",date(), "\n")
 }
 
 
