6613a8bf674f3d4fe26c115ca1970479553df80b
ChristofSeiler <christof.seiler@gmail.com>
2016-10-17 16:13:40 -0700

    flush output

--- R/covreg.mcmc.R
+++ R/covreg.mcmc.R
@@ -95,7 +95,7 @@ b1.save[,,ns/nthin]=B1
 b2.save[,,,ns/nthin]=B2
 a.save[,,ns/nthin]=A
 }
-if (verb==T) cat("iteration",ns,"of",niter,"done at",date(), "\n")
+if (verb==T) cat("iteration",ns,"of",niter,"done at",date(), "\n"); flush.console()
 }
 
 
